74ed416fe6e56a4ac620a4fb22e3c06bd9124ca4
ivainqueur <ishimvainqueur@gmail.com>
2024-01-31 19:59:01 +0200

    fix: removing running errors

--- theme.config.jsx
+++
@@ -1,7 +0,0 @@
-export default {
-    logo: <span>Kin-Lan Documentation</span>,
-    project: {
-      link: 'https://github.com/kin-lang/kin'
-    }
-    // ... other theme options
-  }
---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config = {
+  logo: <span>Kin-Lang Docs</span>,
+  project: {
+    link: 'https://github.com/kin-lang/kin',
+  },
+  docsRepositoryBase: 'https://github.com/kin-lang/wiki',
+  footer: {
+    text: 'Made with 💖 by Murangwa Pacifique.',
+  },
+}
+
+export default config
