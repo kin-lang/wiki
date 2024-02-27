719bb71abb3a70154cbfd5dc22481257a1e248bb
Murangwa Pacifique <96472682+pacifiquem@users.noreply.github.com>
2024-02-27 07:58:34 +0200

    fix: broken link for `edit page on github`

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,7 +3,7 @@ const config = {
   project: {
     link: "https://github.com/kin-lang/kin",
   },
-  docsRepositoryBase: "https://github.com/kin-lang/wiki",
+  docsRepositoryBase: "https://github.com/kin-lang/wiki/tree/main/",
   footer: {
     text: "Made and Designed with 💖 by Murangwa Pacifique.",
   },
