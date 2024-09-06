6d0321e03ca427e8a16fe0f977c58cc73a6c6533
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2024-09-06 11:39:51 +0200

    fix: styles application issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./pages/**/*.{js,jsx,ts,tsx,md,mdx}",
-    "./components/**/*.{js,jsx,ts,tsx,md,mdx}",
-    "./src/**/*.{js,jsx,ts,tsx,md,mdx}",
+    "./**/*.{js,jsx,ts,tsx,md,mdx}",
   ],
   theme: {
     extend: {
