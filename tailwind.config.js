cf19c8acb4092f7aeadf56fc74fe7072374b26a4
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2025-11-03 13:01:15 +0200

    fix: dark theme issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/**/*.{js,jsx,ts,tsx,md,mdx}'
   ],
